6e0bf0d27e24c41a0ad6d1c9dd4de44fcdc2ff29
Fopentos <fopentos@gmail.com>
2026-05-15 22:20:11 +0300

    Update states.py

--- states.py
+++ states.py
@@ -1,11 +1,15 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class OrderStates(StatesGroup):
-    """Состояния для процесса оформления заказа"""
-    waiting_for_link = State()      # ожидание ввода ссылки
-    waiting_for_quantity = State()  # ожидание ввода количества
-    confirm = State()               # подтверждение заказа
+    """Состояния для стандартного заказа (ссылка + количество)"""
+    waiting_for_link = State()
+    waiting_for_quantity = State()
+    confirm = State()
 
 class DepositStates(StatesGroup):
     """Состояние для пополнения баланса"""
-    waiting_for_stars_amount = State()  # ожидание ввода суммы в Stars
+    waiting_for_stars_amount = State()
+
+class DynamicOrderStates(StatesGroup):
+    """Состояние для динамического заказа (много полей, например, голосования, бусты, кастомные комментарии)"""
+    waiting_for_field = State()
